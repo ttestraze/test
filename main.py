c6c886098792c3615d34147b9a9198de13c0fc96
mm <t.bezy2005@gmail.com>
2024-01-20 19:10:17 +0330

    initias commit

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm , Comment
+import os
+
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("app_con_URI", "'8BYkEfBA6O6donzWlSihBXox7C0sKR6b'")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -27,7 +30,7 @@ def load_user(user_id):
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy()
 db.init_app(app)
 
@@ -233,4 +236,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5003)
+    app.run(debug=False, port=5003)
